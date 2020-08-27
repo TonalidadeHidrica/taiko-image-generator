ff01b265542da5d56f1d4c191c76fd2fc83c3a81
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-08-27 18:12:18 +0900

    Add ImageMagick engivornment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use magick_rust::{magick_wand_genesis, MagickWand};
+
+fn main() -> Result<(), &str>{
+    magick_wand_genesis();
+    let wand = MagickWand::new();
+    wand.set_size(1920, 1080)?;
+    wand.read_image("xc:transparent")?;
+    wand.write_image("result.png")?;
+    Ok(())
 }
